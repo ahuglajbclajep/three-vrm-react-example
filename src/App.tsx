b19238acf63a73954156fc69d3c83c57c90ed0cf
ahuglajbclajep <ahuglajbclajep@gmail.com>
2020-03-07 23:09:16 +0900

    use three-vrm

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,27 @@
-import React, { useRef } from "react";
-import { Canvas, useFrame } from "react-three-fiber";
+import React from "react";
+import { Canvas } from "react-three-fiber";
+import { useVRM } from "./hooks";
 
 const App: React.FC = () => {
-  const cube = useRef<import("three").Mesh>(null);
-  useFrame(() => {
-    if (cube.current) {
-      cube.current.rotation.x += 0.01;
-      cube.current.rotation.y += 0.01;
-    }
-  });
+  const [vrm, loadVRM] = useVRM();
+
+  const handleFileChange = (
+    event: React.ChangeEvent<HTMLInputElement>
+  ): void => {
+    // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+    const url = URL.createObjectURL(event.target.files![0]);
+    loadVRM(url);
+  };
+
   return (
-    <mesh ref={cube}>
-      <boxBufferGeometry attach="geometry" args={[1, 1, 1]} />
-      <meshBasicMaterial attach="material" color={0x00ff00} />
-    </mesh>
+    <>
+      <input type="file" accept=".vrm" onChange={handleFileChange} />
+      <Canvas>
+        <directionalLight />
+        {vrm && <primitive object={vrm.scene} />}
+      </Canvas>
+    </>
   );
 };
 
-// see https://github.com/react-spring/react-three-fiber/issues/253
-const Wrapper: React.FC = () => {
-  return (
-    <Canvas>
-      <App />
-    </Canvas>
-  );
-};
-
-export default Wrapper;
+export default App;
---
+++ src/hooks.ts
@@ -0,0 +1,18 @@
+import { VRM } from "@pixiv/three-vrm";
+import { useRef, useState } from "react";
+import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader";
+
+const useVRM = (): [VRM | null, (_: string) => void] => {
+  const { current: loader } = useRef(new GLTFLoader());
+  const [vrm, setVRM] = useState<VRM | null>(null);
+
+  const loadVRM = (url: string): void => {
+    loader.load(url, gltf => {
+      VRM.from(gltf).then(vrm => setVRM(vrm));
+    });
+  };
+
+  return [vrm, loadVRM];
+};
+
+export { useVRM };
