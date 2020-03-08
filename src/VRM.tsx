a2887ff8b40a5292c8de4bc8e91e07a0f5dad629
ahuglajbclajep <ahuglajbclajep@gmail.com>
2020-03-08 23:58:41 +0900

    move eyes

--- src/VRM.tsx
+++ src/VRM.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { useFrame } from "react-three-fiber";
+import { Vector3 } from "three";
 
 type Props = {
   vrm: import("@pixiv/three-vrm").VRM | null;
@@ -7,10 +8,12 @@ type Props = {
 
 // see https://github.com/react-spring/react-three-fiber/issues/253
 const VRM: React.FC<Props> = ({ vrm }) => {
-  useFrame(({ clock }, delta) => {
+  useFrame(({ clock, mouse }, delta) => {
     if (vrm) {
       // see https://github.com/FMS-Cat/three-vrm-vtuber/tree/03890a8/step3
       vrm.scene.rotation.y = Math.PI * Math.sin(clock.getElapsedTime());
+      if (vrm.lookAt) vrm.lookAt.lookAt(new Vector3(mouse.x, mouse.y, 0));
+
       vrm.update(delta);
     }
   });
