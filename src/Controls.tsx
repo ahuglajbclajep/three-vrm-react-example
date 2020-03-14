dcb3ecce065eb3bd6c39959b5af59d74d826b913
ahuglajbclajep <ahuglajbclajep@gmail.com>
2020-03-14 21:56:07 +0900

    fix typedef

--- src/Controls.tsx
+++ src/Controls.tsx
@@ -1,8 +1,23 @@
 import React, { useRef } from "react";
-import { extend, useFrame, useThree } from "react-three-fiber";
+import { extend, ReactThreeFiber, useFrame, useThree } from "react-three-fiber";
+import { Vector3 } from "three";
 import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
 
 extend({ OrbitControls });
+
+// see https://github.com/react-spring/react-three-fiber/issues/11#issuecomment-583599378
+declare global {
+  // eslint-disable-next-line @typescript-eslint/no-namespace
+  namespace JSX {
+    interface IntrinsicElements {
+      orbitControls: ReactThreeFiber.Object3DNode<
+        OrbitControls,
+        // `OrbitControls["new"]` does not work without `typeof`.
+        typeof OrbitControls
+      >;
+    }
+  }
+}
 
 // see https://github.com/react-spring/react-three-fiber/blob/v4.0.20/examples/src/demos/dev/GltfAnimation.js#L39-L44
 const Controls: React.FC = () => {
@@ -12,11 +27,11 @@ const Controls: React.FC = () => {
   useFrame(() => controls.current?.update());
 
   return (
-    // @ts-ignore
     <orbitControls
       ref={controls}
       args={[camera, gl.domElement]}
       enableDamping
+      target={new Vector3(0, 1, -2)}
     />
   );
 };
