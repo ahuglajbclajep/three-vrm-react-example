5a76cf2133743103fafda460ab9f795156e5b61c
ahuglajbclajep <ahuglajbclajep@gmail.com>
2020-03-14 21:51:46 +0900

    use extend

--- src/Controls.tsx
+++ src/Controls.tsx
@@ -1,18 +1,24 @@
-import React, { useEffect, useRef } from "react";
-import { useThree } from "react-three-fiber";
+import React, { useRef } from "react";
+import { extend, useFrame, useThree } from "react-three-fiber";
 import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
 
-// `extend({ OrbitControls });` (also used in examples) not working.
-// see https://github.com/react-spring/react-three-fiber/issues/278
+extend({ OrbitControls });
+
+// see https://github.com/react-spring/react-three-fiber/blob/v4.0.20/examples/src/demos/dev/GltfAnimation.js#L39-L44
 const Controls: React.FC = () => {
+  const controls = useRef<OrbitControls>(null);
   const { camera, gl } = useThree();
-  const { current: controls } = useRef(
-    new OrbitControls(camera, gl.domElement)
+
+  useFrame(() => controls.current?.update());
+
+  return (
+    // @ts-ignore
+    <orbitControls
+      ref={controls}
+      args={[camera, gl.domElement]}
+      enableDamping
+    />
   );
-
-  useEffect(() => (): void => controls.dispose(), [controls]);
-
-  return null;
 };
 
 export default Controls;
