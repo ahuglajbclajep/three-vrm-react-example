a427e8397dd700c78c025d703afbafd9bafd512a
ahuglajbclajep <ahuglajbclajep@gmail.com>
2020-03-29 09:25:34 +0900

    little fix

--- src/hooks.ts
+++ src/hooks.ts
@@ -6,20 +6,23 @@ const useVRM = (): [VRM | null, (url: string) => Promise<void>] => {
   const { current: loader } = useRef(new GLTFLoader());
   const [vrm, setVRM] = useState<VRM | null>(null);
 
-  const loadVRM = (url: string): Promise<void> =>
-    new Promise((resolve: (_: GLTF) => void) => loader.load(url, resolve))
-      .then((gltf) => VRM.from(gltf))
-      .then((vrm) => {
-        vrm.scene.rotation.y = Math.PI;
-        setVRM(vrm);
-      });
+  const loadVRM = useCallback(
+    (url: string): Promise<void> =>
+      new Promise((resolve: (_: GLTF) => void) => loader.load(url, resolve))
+        .then((gltf) => VRM.from(gltf))
+        .then((vrm) => {
+          vrm.scene.rotation.y = Math.PI;
+          setVRM(vrm);
+        }),
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+    []
+  );
 
   return [vrm, loadVRM];
 };
 
 const useToggle = (initialState: boolean): [boolean, () => void] => {
   const [state, setState] = useState(initialState);
-
   const toggle = useCallback(() => setState((prev) => !prev), []);
 
   return [state, toggle];
